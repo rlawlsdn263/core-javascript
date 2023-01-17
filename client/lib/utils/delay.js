3b15df550074daa0d9b0bdb8256da6358cca186c
rlawlsdn263 <rlawlsdn263@naver.com>
2023-01-17 16:11:45 +0900

    [추가] xhr

---
+++ client/lib/utils/delay.js
@@ -0,0 +1,11 @@
+import { getNode } from "../dom/getNode.js";
+
+const first = getNode(".first");
+
+console.log(first);
+
+function
+
+first.style.top = "-100px";
+first.style.transform = "rotate(360deg)";
+first.style.top = "0px";
--- client/main.js
+++ client/main.js
@@ -1,2 +1,11 @@
-console.log("Hello World!");
-console.log(window);
+import { insertLast, xhrData } from "./lib/index.js";
+
+xhrData.get(
+  "https://jsonplaceholder.typicode.com/users/1",
+  (res) => {
+    insertLast("body", JSON.stringify(res));
+  },
+  (err) => {
+    insertLast("body", "데이터 로딩에 실패했습니다.");
+  }
+);
