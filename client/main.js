c80dc70787df1b7a105780fe0011850b0cef69dd
rlawlsdn263 <rlawlsdn263@naver.com>
2023-01-18 21:54:12 +0900

    onSuccess, onFail 콜백함수

--- client/main.js
+++ client/main.js
@@ -1,89 +1,9 @@
-/* 
-//xhr
-
-const xhr = new XMLHttpRequest();
-//console.log(xhr);
-
-//비동기 통신 오픈
-xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-
-xhr.addEventListener("readystatechange", () => {
-  //console.log(xhr.readyState);
-  //console.log(xhr.status);
-
-  //xhr.status가 200이상 400미만이면 실행
-  if (xhr.status >= 200 && xhr.status < 400) {
-    if (xhr.readyState === 4) {
-      console.log("통신 성공");
-    }
-  } else {
-    console.log("통신 실패");
-  }
-});
-
-//서버 요청
-xhr.send(); 
-*/
-
-/* //유틸함수 만들기
-
-function xhrData(method, url, body) {
-  const xhr = new XMLHttpRequest();
-
-  xhr.open(method, url);
-
-  xhr.addEventListener("readystatechange", () => {
-    if (xhr.status >= 200 && xhr.status < 400) {
-      if (xhr.readyState === 4) {
-        console.log("통신 성공");
-        console.log(JSON.parse(xhr.response));
-      }
-    } else {
-      console.log("통신 실패");
-    }
-  });
-
-  xhr.send(JSON.stringify(body));
-}
-
-//GET 요청
-xhrData("GET", "https://jsonplaceholder.typicode.com/users");
-
-//POST 요청
-xhrData("POST", "https://jsonplaceholder.typicode.com/users", {
-  name: "진우",
-  username: "지누옹",
-  email: "rlawlsdn263@naver.com",
-}); */
-
-/* //구조분해할당자
-function xhrData(method, url, body) {
-  const xhr = new XMLHttpRequest();
-
-  xhr.open(method, url);
-
-  xhr.addEventListener("readystatechange", () => {
-    const { status, readyState, response } = xhr;
-
-    if (status >= 200 && xhr.status < 400) {
-      if (readyState === 4) {
-        console.log("통신 성공");
-        console.log(JSON.parse(response));
-      }
-    } else {
-      console.log("통신 실패");
-    }
-  });
-
-  xhr.send(JSON.stringify(body));
-}
- */
-
-//
 function xhrData({
   url = "",
   method = "GET",
   body = null,
+  onSuccess = null,
+  onFail = null,
   headers = { "Content-Type": "application/json" },
 }) {
   // const {url, method, body} = options;
@@ -101,11 +21,12 @@ function xhrData({
 
     if (status >= 200 && xhr.status < 400) {
       if (readyState === 4) {
-        console.log("통신 성공");
+        // console.log("통신 성공");
         console.log(JSON.parse(response));
       }
     } else {
-      console.log("통신 실패");
+      // console.log("통신 실패");
+      onFail("통신 실패");
     }
   });
 
@@ -113,5 +34,11 @@ function xhrData({
 }
 
 xhrData({
-  url: "https://jsonplaceholder.typicode.com/users",
+  url: "https://jsonplaceholder.typicode.com/users/1",
+  onSuccess: (result) => {
+    console.log(result);
+  },
+  onFail: (err) => {
+    console.error(err);
+  },
 });
