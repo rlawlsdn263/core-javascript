839e64cba1f86898a71e39a896c330572f427e60
rlawlsdn263 <rlawlsdn263@naver.com>
2023-01-18 22:46:17 +0900

    then, catch

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 let promise = new Promise(function (resolve, reject) {
-  setTimeout(() => resolve("통신 성공!"), 3000);
-  // setTimeout(() => reject(new Error("에러 발생")), 3000);
+  // setTimeout(() => resolve("통신 성공!"), 3000);
+  setTimeout(() => reject(new Error("에러 발생")), 3000);
 });
 
-console.log(promise);
+promise.then((res) => console.log(res)).catch((err) => console.log(err));
